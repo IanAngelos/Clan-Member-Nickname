6bf2b3d4f7563441228821c8d4c9f52a9820478a
IanAngelos <ianangelos@hotmail.com>
2022-09-24 18:50:14 -0500

    Cleaned Code

--- src/main/java/com/cmn/Constants.java
+++ src/main/java/com/cmn/Constants.java
@@ -1,7 +1,19 @@
 package com.cmn;
 
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.sheets.v4.SheetsScopes;
+
+import java.util.Collections;
+import java.util.List;
+
 public class Constants {
     public static final String SHEET_ID = "1IZKSdin6ovX1jiknsIpbOkrP5qwUeWKrWMM4sbOGPPE";
+    public static final String SHEET_RANGE = "Clan Nicknames!A:B";
     public static final String CREDENTIALS_FILE_PATH = "/credentials.json";
     public static final String APPLICATION_NAME = "Clan Member Nickname";
+    public static final String TOKENS_DIRECTORY_PATH = "tokens";
+    public static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    public static final List<String> SCOPES =
+            Collections.singletonList(SheetsScopes.SPREADSHEETS);
 }
--- src/main/java/com/cmn/SheetsTest.java
+++ src/main/java/com/cmn/SheetsTest.java
@@ -7,82 +7,55 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.ValueRange;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.cmn.Constants.SHEET_ID;
-
 public class SheetsTest {
-    private static final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
-    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
-
-    /**
-     * Global instance of the scopes required by this quickstart.
-     * If modifying these scopes, delete your previously saved tokens/ folder.
-     */
-    private static final List<String> SCOPES =
-            Collections.singletonList(SheetsScopes.SPREADSHEETS);
-    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
-
-    /**
-     * Creates an authorized Credential object.
-     *
-     * @param HTTP_TRANSPORT The network HTTP Transport.
-     * @return An authorized Credential object.
-     * @throws IOException If the credentials.json file cannot be found.
-     */
     private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
             throws IOException {
         // Load client secrets.
-        InputStream in = SheetsTest.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        InputStream in = SheetsTest.class.getResourceAsStream(Constants.CREDENTIALS_FILE_PATH);
         if (in == null) {
-            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+            throw new FileNotFoundException("Resource not found: " + Constants.CREDENTIALS_FILE_PATH);
         }
         GoogleClientSecrets clientSecrets =
-                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+                GoogleClientSecrets.load(Constants.JSON_FACTORY, new InputStreamReader(in));
 
         // Build flow and trigger user authorization request.
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                HTTP_TRANSPORT, Constants.JSON_FACTORY, clientSecrets, Constants.SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(Constants.TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
-    /**
-     * Prints the names and majors of students in a sample spreadsheet:
-     * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-     */
     public static void main(String... args) throws IOException, GeneralSecurityException {
         // Build a new authorized API client service.
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        final String spreadsheetId = SHEET_ID;
-        final String range = "Clan Nicknames!A:B";
-        Map<String, String> clanNameMapRead = new HashMap<>();
         Sheets service =
-                new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                        .setApplicationName(APPLICATION_NAME)
+                new Sheets.Builder(HTTP_TRANSPORT, Constants.JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                        .setApplicationName(Constants.APPLICATION_NAME)
                         .build();
         ValueRange response = service.spreadsheets().values()
-                .get(spreadsheetId, range)
+                .get(Constants.SHEET_ID, Constants.SHEET_RANGE)
                 .execute();
+        setClanNameMapRead(response);
+    }
+
+    private static void setClanNameMapRead(ValueRange response){
         List<List<Object>> values = response.getValues();
+        Map<String, String> clanNameMapRead = new HashMap<>();
         if (values == null || values.isEmpty()) {
             System.out.println("No data found.");
         } else {
