65fe895850c3b7877b2e9ac0ab33eb623d996fd4
IanAngelos <ianangelos@hotmail.com>
2022-09-24 17:16:26 -0500

    Adding Google Sheets implementation with creds.

--- src/main/java/com/cmn/Configuration.java
+++ src/main/java/com/cmn/Configuration.java
@@ -1,19 +1,19 @@
-package com.cmn;
-
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
-
-@ConfigGroup("clanmembernicknmame")
-public interface Configuration extends Config
-{
-	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
-	)
-	default String greeting()
-	{
-		return "Hello";
-	}
-}
+//package com.cmn;
+//
+//import net.runelite.client.config.Config;
+//import net.runelite.client.config.ConfigGroup;
+//import net.runelite.client.config.ConfigItem;
+//
+//@ConfigGroup("clanmembernicknmame")
+//public interface Configuration extends Config
+//{
+//	@ConfigItem(
+//		keyName = "greeting",
+//		name = "Welcome Greeting",
+//		description = "The message to show to the user when they login"
+//	)
+//	default String greeting()
+//	{
+//		return "Hello";
+//	}
+//}
